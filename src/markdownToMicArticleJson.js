c9a46b2e0fc80fcb92972783dd406f8fc5686a65
Daniel Scanlon <dscanlonpa@gmail.com>
2018-09-04 11:50:53 -0400

    Init html-to-article-json

--- src/markdownToMicArticleJson.js
+++ src/markdownToMicArticleJson.js
@@ -1,5 +1,7 @@
-const htmlToArticleJson = require('html-to-article-json');
+const HtmlToArticleJson = require('html-to-article-json');
 const markdownToHtml = require('./markdownToHtml');
+
+const htmlToArticleJson = HtmlToArticleJson(); // use default options
 
 // markdown -> micArticleJson
 module.exports = function markdownToMicArticleJson(markdown) {
